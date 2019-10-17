1b75de5e95ae66811beb3acb474461682283627c
SUMUKHNAIK <31795364+SUMUKHNAIK@users.noreply.github.com>
2019-10-17 08:43:25 -0700

    Update adminvalidation.php

--- adminvalidation.php
+++ adminvalidation.php
@@ -1,23 +1,34 @@
 <?php
-
 session_start();
 require 'configure.php';
 if(isset($_POST['signup-submit'])){
 	$Name = $_POST['admin'];
 	$password = $_POST['password'];
-	$q = "select * from admin_login where admin= '$Name' && password = '$password';";
-	$result = mysqli_query($conn,$q);
+	$query = "select * from admin_login where admin= '$Name' && password = '$password';";
+	$result = mysqli_query($conn,$query);
 	$num = mysqli_num_rows($result);
-	if($num==1){
-		$_SESSION['name'] = $Name;
-		header('location:adminhome.php');
+	if($num!=0){
+		while($row = mysqli_fetch_assoc($result))
+		{
+			$dbusername=$row['admin'];
+			$dbpassword=$row['password'];
+		}
+		if($Name == $dbusername && $password == $dbpassword)
+		{
+			session_start();
+			$_SESSION['sess_user']=$Name;
+			//Redirect Browser
+			header("Location:adminhome.php");
+		}
 	}
-	else if($num==0){
+	else{
 		echo "User doesnt exist";
-		//header('location:adminlogin.php');
+		header('location:adminlogin.php');
 	}
 }
+
 else{
+	echo "User doesnt exist";
 	header('location:adminlogin.php');
 }
 ?>

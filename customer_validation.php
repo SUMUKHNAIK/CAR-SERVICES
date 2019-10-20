bf995286b964b76ac04cd5873137a38714d2bb95
SUMUKHNAIK <31795364+SUMUKHNAIK@users.noreply.github.com>
2019-10-20 01:53:50 -0700

    Update customer_validation.php

--- customer_validation.php
+++ customer_validation.php
@@ -1,20 +1,60 @@
 <?php
-
 session_start();
-
-$con = mysqli_connect('localhost','root');
-if($con){
-    echo "Connection successful";
-}else{
-    echo "Failed to connect";
+require 'configure.php';
+if(isset($_POST['signup'])){
+	$Name = $_POST['user_name'];
+	$password = $_POST['password1'];
+	
+	$query = "select * from customer_signup where user_name= '$Name' && password1 = '$password';";
+	$result = mysqli_query($conn,$query);
+	$num = mysqli_num_rows($result);
+	if($num!=0){
+		while($row = mysqli_fetch_assoc($result))
+		{
+			$dbusername=$row['user_name'];
+			$dbpassword=$row['password1'];
+		}
+		if($Name == $dbusername && $password == $dbpassword)
+		{
+			session_start();
+			$id = "select cust_id from customer_signup where user_name=$Name;";
+			$_SESSION['sess_user']=$Name;
+			//Redirect Browser
+			header("Location:customerhome.php");
+		}
+	}
+	else{
+		echo "User doesnt exist";
+		header('location:home.php');
+	}
 }
+else{
+	header('location:home.php');
+}
+?>
 
-mysqli_select_db($con,'carservices');
+/*
+session_start();
+require 'configure.php';
 
 $Name = $_POST['user_name'];
 $password = $_POST['password'];
-$hashpwd= password_hash($password,PASSWORD_DEFAULT);
-if(password_verify($password,$hashpwd)
+$q = "select * from customer_signup where user_name= '$Name' && password1 = '$password';";
+	$result = mysqli_query($conn,$q);
+	$num = mysqli_num_rows($result);
+	if($num==1){
+		$_SESSION['name'] = $Name;
+		header('location:adminhome.php');
+	}
+	else if($num==0){
+		echo "User doesnt exist";
+		//header('location:adminlogin.php');
+	}
+else{
+	header('location:adminlogin.php');
+}*/
+
+/*
 {
 	$q = "select * from customer_signup where user_name= '$Name';";
 	$result = mysqli_query($con,$q);
@@ -28,3 +68,4 @@ else{
 	echo "Invalid Password";
 }
 ?>
+*/

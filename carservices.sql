13fd81b864e6bc1cec1dc836c83ac74a041dc8f2
SUMUKHNAIK <31795364+SUMUKHNAIK@users.noreply.github.com>
2019-10-20 01:53:01 -0700

    Update carservices.sql

--- carservices.sql
+++ carservices.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2019 at 02:46 PM
+-- Generation Time: Oct 20, 2019 at 10:40 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -44,6 +44,30 @@ INSERT INTO `admin_login` (`admin_id`, `admin`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `Booking_id` int(30) NOT NULL,
+  `cust_id` int(30) NOT NULL,
+  `cust_name` varchar(30) NOT NULL,
+  `contact` bigint(11) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `ServiceType` varchar(50) NOT NULL,
+  `car_reg_no` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`Booking_id`, `cust_id`, `cust_name`, `contact`, `address`, `ServiceType`, `car_reg_no`) VALUES
+(3, 1, 'abhi', 1234567891, 'ssssasasasas', 'Car Wash', 'KA01AB1501'),
+(4, 17, 'rocky', 7760712345, 'Shivajinagar,Bangalore', 'Car wash', 'KA01AB1501');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `customer_signup`
 --
 
@@ -53,8 +77,8 @@ CREATE TABLE `customer_signup` (
   `email` varchar(20) NOT NULL,
   `password1` varchar(20) NOT NULL,
   `password2` varchar(20) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  `contact` int(10) NOT NULL
+  `address` varchar(100) NOT NULL,
+  `contact` bigint(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -63,10 +87,9 @@ CREATE TABLE `customer_signup` (
 
 INSERT INTO `customer_signup` (`cust_id`, `user_name`, `email`, `password1`, `password2`, `address`, `contact`) VALUES
 (1, 'abhi', 'abhi05@gmail.com', 'pass123', 'pass123', 'sasasas', 1234567891),
-(5, 'arman', 'arman.1@gmail.com', '0', '$2y$10$f11ClyWYayBjj', 'BTM LAYOUT BANGA:LORE', 1211234562),
-(6, 'alok', 'alokkumar545@gmail.c', '0', '$2y$10$l4kmFR.9BcK.l', 'BTM LAYOUT BANGA:LORE', 1211035562),
-(8, 'Aran', 'aran.1@gmail.com', '0', '$2y$10$/o498tznSIhLZ', 'BTM LAYOUT BANGA:LORE', 2147483647),
-(9, 'smaran', 'smaransep05@gmail.co', '0', '$2y$10$icxxffuCelI64', 'Thirthahalli shivamogga', 2147483647);
+(16, 'sujith', 'sujithn14@gmail.com', '$2y$10$d52MdeC95l30k', '$2y$10$d52MdeC95l30k', 'Bomanahalli,Bangalore', 9878564512),
+(17, 'rocky', 'rockypes@gmail.com', 'rocky123', 'rocky123', 'Shivajinagar,Bangalore', 7760712345),
+(18, 'karthik', 'karthikpes@gmail.com', 'karthik123', 'karthik123', 'Jayanagar,Bengaluru', 7894561210);
 
 -- --------------------------------------------------------
 
@@ -77,7 +100,7 @@ INSERT INTO `customer_signup` (`cust_id`, `user_name`, `email`, `password1`, `pa
 CREATE TABLE `dealers_info` (
   `id` int(20) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `job` varchar(11) NOT NULL,
+  `job` varchar(40) NOT NULL,
   `contact` bigint(11) NOT NULL,
   `address` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -87,7 +110,12 @@ CREATE TABLE `dealers_info` (
 --
 
 INSERT INTO `dealers_info` (`id`, `name`, `job`, `contact`, `address`) VALUES
-(1, 'Anandh', 'Mechanic', 7886532101, 'Kalasipaalya, Bangalore');
+(1, 'Anandh', 'Mechanic', 7886532101, 'Kalasipaalya, Bangalore'),
+(2, 'Bharath', 'Engine Mechanic', 6785123410, 'Bannerughata,Bengaluru'),
+(3, 'Vikas', 'Car Wash dealer', 7785123410, 'RR nagar,Bengaluru'),
+(4, 'Sujith', 'Car paint dealer', 9448524169, 'GarebhaaviPaalya,Bengaluru'),
+(5, 'Karthik', 'Mechanic', 8054321101, 'Electronic City,Bengaluru'),
+(6, 'Jack', 'General motor Service test', 6654321210, 'Shivajinagar,Bangalore');
 
 --
 -- Indexes for dumped tables
@@ -98,6 +126,13 @@ INSERT INTO `dealers_info` (`id`, `name`, `job`, `contact`, `address`) VALUES
 --
 ALTER TABLE `admin_login`
   ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`Booking_id`),
+  ADD KEY `cust_id` (`cust_id`);
 
 --
 -- Indexes for table `customer_signup`
@@ -122,16 +157,33 @@ ALTER TABLE `admin_login`
   MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `Booking_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `customer_signup`
 --
 ALTER TABLE `customer_signup`
-  MODIFY `cust_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `cust_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `dealers_info`
 --
 ALTER TABLE `dealers_info`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`cust_id`) REFERENCES `customer_signup` (`cust_id`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`cust_id`) REFERENCES `customer_signup` (`cust_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

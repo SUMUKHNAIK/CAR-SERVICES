ebe42e319cc725fc2c11e1c12bd9dbed4747820b
SUMUKHNAIK <31795364+SUMUKHNAIK@users.noreply.github.com>
2019-10-20 01:55:18 -0700

    Update dealers.php

--- dealers.php
+++ dealers.php
@@ -1,56 +1,64 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Dealers information</title>
-<meta content="noindex, nofollow" name="robots">
-<link href="style1.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<div class="maindiv">
-<div class="divA">
-<div class="title">
-<h2>Dealers information</h2>
-</div>
-<div class="divB">
-<div class="divD">
-<p>Click On Menu</p>
+<link rel="stylesheet" href="style1.css">
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="bootstrap.min.js"></script>
+<div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+			<a href="AddDealer.php" class="nav-item nav-link">Add Dealer</a>
+			<a href="DeleteDealer.php" class="nav-item nav-link">Delete Dealer</a>
+			<a href="adminhome.php" class="nav-item nav-link">Back</a>
+			
+        </div>
+    </div>
+<style type="text/css">
+
+	a{
+        font-size: 25px;
+        padding: 10px;
+		color: red;
+	}
+	body {
+		background-image: url(dealerimg.jpg);
+		height= 50%;
+		background-repeat: no-repeat;
+		background-size: cover;
+		color: black;
+		font-size: 20px;
+	}
+	
+</style>
 <?php
 require 'configure.php';
-//MySQL Query to read data
-$q1="select * from dealers_info;";
-$query = mysqli_query($q1,$conn);
-while ($row = mysqli_fetch_assoc($query)) {
-	echo "<b><a href="dealers.php? id={$row['id']}" > {$row['name']} </a></b>";
-	echo "<br />";
-}
+$result = mysqli_query($conn,"SELECT * FROM dealers_info;");
+
+echo "<table border='1'>
+<tr>
+<th>id</th>
+<th>name</th>
+<th>job</th>
+<th>contact</th>
+<th>address</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['id'] . "</td>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['job'] . "</td>";
+  echo "<td>" .$row['contact'] ."</td>";
+  echo "<td>" .$row['address'] ."</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysqli_close($conn);
 ?>
-</div>
-<?php
-if (isset($_GET['id'])) {
-$id = $_GET['id'];
-$q="select * from dealers_info where id=$id";
-$query1 = mysqli_query($q,$conn);
-while ($row1 = mysqli_fetch_array($query1)) {
-?>
-<div class="form">
-<h2>---Details---</h2>
-<!-- Displaying Data Read From Database -->
-<span>Name:</span> <?php echo $row1['name']; ?>
-<span>Job:</span> <?php echo $row1['job']; ?>
-<span>Contact No:</span> <?php echo $row1['contact']; ?>
-<span>Address:</span> <?php echo $row1['address']; ?>
-</div>
-<?php
-}
-}
-?>
-<div class="clear"></div>
-</div>
-<div class="clear"></div>
-</div>
-</div>
-<?php
-mysqli_close($conn); // Closing Connection with Server
-?>
-</body>
-</html>
+</head>
+
+<html>
